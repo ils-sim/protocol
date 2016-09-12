5e6cf9e190519bcaa619c25fe088d97ed747f19d
Peter Buchegger <peter@pbuchegger.at>
2016-09-13 00:13:24 +0200

    first travis try

--- install_protobuf.sh
+++ install_protobuf.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env sh
+set -ex
 wget https://github.com/google/protobuf/releases/download/v3.0.0-beta-3/protobuf-cpp-3.0.0-beta-3.zip
 unzip protobuf-cpp-3.0.0-beta-3.zip
 cd protobuf-3.0.0-beta-3/
 ./configure
 make
-make check
 sudo make install
