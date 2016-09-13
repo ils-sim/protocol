b3bbd4ff70e2cb5df85c03df107b2222e8b35fe9
Peter Buchegger <peter@pbuchegger.at>
2016-09-13 22:16:56 +0200

    travis cache test

--- install_protobuf.sh
+++ install_protobuf.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 set -ex
-wget https://github.com/google/protobuf/releases/download/v3.0.0-beta-3/protobuf-cpp-3.0.0-beta-3.zip
-unzip protobuf-cpp-3.0.0-beta-3.zip
-cd protobuf-3.0.0-beta-3/
-./configure
-make
-sudo make install
+
+if [ ! -d "$HOME/protobuf/lib" ]; then
+	wget https://github.com/google/protobuf/releases/download/v3.0.0-beta-3/protobuf-cpp-3.0.0-beta-3.zip
+	unzip protobuf-cpp-3.0.0-beta-3.zip
+	cd protobuf-3.0.0-beta-3/
+	./configure
+	make
+	sudo make install
+else
+  echo "Using cached directory."
+fi
