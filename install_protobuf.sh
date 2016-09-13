e16f716e2d81b7980881cac8f880d6eed298aaa6
Peter Buchegger <peter@pbuchegger.at>
2016-09-13 22:23:25 +0200

    another test

--- install_protobuf.sh
+++ install_protobuf.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/protobuf/lib" ]; then
 	wget https://github.com/google/protobuf/releases/download/v3.0.0-beta-3/protobuf-cpp-3.0.0-beta-3.zip
 	unzip protobuf-cpp-3.0.0-beta-3.zip
 	cd protobuf-3.0.0-beta-3/
-	./configure
+	./configure --prefix=$HOME/protobuf
 	make
 	sudo make install
 else
